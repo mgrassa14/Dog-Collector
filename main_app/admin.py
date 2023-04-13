c52c2b30340608979b12751888e85a474ae20076
Maria Grassa <mgrassa00gmail.com>
2023-04-12 19:26:52 -0700

    M:M functionality works

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Dog, Feeding
+from .models import Dog, Feeding, Toy
 # Add our Dog model to the admin site, so we can perform
 # CRUD operations on it!
 admin.site.register(Dog)
 # register the new Feeding Model 
 admin.site.register(Feeding)
+# register the new Toy Model 
+admin.site.register(Toy)
--- main_app/views.py
+++ main_app/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 # Create your views here.
 from django.http import HttpResponse
 
-from .models import Dog
+from .models import Dog, Toy
 # Import the FeedingForm
 from .forms import FeedingForm
 
 class DogCreate(CreateView):
   model = Dog
-  fields = '__all__'
+  fields = ['name', 'breed', 'description', 'age']
 
 class DogUpdate(UpdateView):
   model = Dog
@@ -24,13 +25,15 @@ class DogDelete(DeleteView):
 def dogs_index(request):
     # key 'dogs' will be the variable name in the dogs/index.html
     # dogs will be the array that we are storing in the 'dogs'
-  dogs = Dog.objects.all() # finding all of the cats from the database!
+  dogs = Dog.objects.all() # finding all of the dogs from the database!
   return render(request, 'dogs/index.html', { 'dogs': dogs })
 
 def dogs_detail(request, dog_id):
-  dog = Dog.objects.get(id=dog_id) # find the cat with the id that was in the params in the db
+  dog = Dog.objects.get(id=dog_id) # find the dog with the id that was in the params in the db
+  # Get the toys the dog doesn't have
+  toys_dog_doesnt_have = Toy.objects.exclude(id__in = dog.toys.all().values_list('id'))
   feeding_form = FeedingForm()
-  return render(request, 'dogs/detail.html', {'dog': dog, 'feeding_form': feeding_form})
+  return render(request, 'dogs/detail.html', {'dog': dog, 'feeding_form': feeding_form, 'toys': toys_dog_doesnt_have})
 
 def add_feeding(request, dog_id):
   # create a ModelForm instance using the data in request.POST
@@ -52,4 +55,29 @@ def about(request):
     # django automatically looks in templates folder for html
     return render(request, 'about.html')
 
+class ToyList(ListView):
+    model = Toy
 
+
+class ToyDetail(DetailView):
+    model = Toy
+
+
+class ToyCreate(CreateView):
+    model = Toy
+    fields = '__all__'
+
+
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys/'
+
+def assoc_toy(request, dog_id, toy_id):
+  # Note that you can pass a toy's id instead of the whole object
+  Dog.objects.get(id=dog_id).toys.add(toy_id)
+  return redirect('detail', dog_id=dog_id)
